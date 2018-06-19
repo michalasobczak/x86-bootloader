8ee51ef7ecd2212f49a74e7155c5bb2b4b281ecd
Michal Sobczak <michalasobczak@gmail.com>
2018-06-19 13:20:40 +0200

    Split divided screen with white and red colouring.

--- main.asm
+++ main.asm
@@ -2,7 +2,7 @@
 ;  Initialization
 ; **************************
 [bits 16]
-[org 0x7c00]
+[org 0x7C00]
 
 ; **************************
 ;  Main
@@ -16,10 +16,10 @@ jmp $
 ;  Procedures
 ; **************************
 PrintSingleCharacter:
-  mov ah, 0x0e
-  mov bh, 0x00
-  mov bl, 0x07
-  int 0x10
+  mov ah, 0x000E
+  mov bh, 0x0000
+  mov bl, 0x0007
+  int 0x0010
   ret
 
 PrintString:
@@ -32,16 +32,30 @@ PrintString:
     jmp NextCharacter
 
 EnterVideoMode:
-  mov ax, 0x13 
-  int 0x10
-  WhileTrue:
+  mov ax, 0x0013 
+  int 0x0010
+  Draw:
+    ; segment
+    mov ax, 0xA000 ; 40960d
+    mov es, ax
+    ; offset 0
+    xor di, di
+    ; 
+    mov ax, 0x0F ; white 
+    mov cx, 0x7D00 ; 32000d
+    rep stosb
+    ;
+    mov ax, 0x04 ; red 
+    mov cx, 0x7D00 ; 32000d
+    rep stosb
+  Control:
     nop
-    jmp WhileTrue
+    jmp Control
   ;call ExitVideoMode
 
 ExitVideoMode:
-  mov ax,0003h 
-  int 10h
+  mov ax,0x0003 
+  int 0x0010
   
 ExitFunction:
   ret
@@ -54,5 +68,5 @@ HelloString db 'Hello, world!', 0
 ; **************************
 ;  Ending
 ; **************************
-TIMES 510 - ($ - $$) db 0
-DW 0xAA55
+TIMES 510 - ($ - $$) db 0 ; zero-fill
+DW 0xAA55 ; boot signature
