17a2a64cf6fa84790fb7980a65d7595f5436c262
Michal Sobczak <michalasobczak@gmail.com>
2018-06-24 08:15:39 +0200

    Ortographic projection WIP

--- main.asm
+++ main.asm
@@ -73,6 +73,27 @@ PrintSingleCharacter:
       cmp ax, [vx1]          
       jle %%.Each
 %endmacro 
+
+%macro Project3DInto2D 3 
+  mov word [x3], %1
+  mov word [y3], %2
+  mov word [z3], %3
+  mov ax, [z3] ; int d = az/30;
+  mov cx, 30   ; ?
+  div cx       ; ?
+  mov [d], ax
+  mov ax, [z3] ; int d2 = az/20;
+  mov cx, 20   ; ?
+  div cx       ; ?
+  mov [d2], ax
+  mov ax, [x3] ; int bx = (ax*1) + d;
+  add ax, [d]
+  mov [x2], ax
+  mov ax, [y3] ; int by = (ay*1) + d2 - 150;
+  add ax, [d2]
+  sub ax, 150
+  mov [y2], ax
+%endmacro 
  
 EnterVideoMode:
   mov ax, 0x0013 
@@ -80,8 +101,9 @@ EnterVideoMode:
   mov ax, 0xA000 ; 40960d
   mov es, ax
   .Draw:
-    LineFromTo 10,10, 135,20    
-    LineFromTo 20,20, 135,20    
+    LineFromTo 10,10, 135,20
+    Project3DInto2D -100+250,-100+40,100+1500
+    .Tst1: PutPixelAt [x2],[y2],0x0f
     jmp .Draw
           
 ;ExitVideoMode:
@@ -100,6 +122,20 @@ vx0: dw 0
 vy0: dw 0
 vx1: dw 5
 vy1: dw 5
+;
+x3:  dw 0
+y3:  dw 0
+z3:  dw 0
+x2:  dw 0
+y2:  dw 0  
+;
+vertices: db -100,-100,100, -100,100,100, -100,-100,-100, -100,100,-100, 100,-100,100,100,100,100,100,-100,-100,100,100,-100
+faces___: db 2,4,3,1,4,8,7,3,8,6,5,7,6,2,1,5,1,3,7,5,6,8,4,2
+model2d_: times 16 db 0
+;
+d:   dw 0
+d2:  dw 0
+;
 ; **************************
 ;  Ending
 ; **************************
